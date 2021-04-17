b4d64c822b84447904cd0c6c3540b1acd11fab8e
이현재 <qazx5024@gmail.com>
2021-04-17 16:07:14 +0900

    update RandomPicker

---
+++ RandomPicker.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPicker<T>
+{
+    public T Pick(T[] candidates)
+    {
+        return candidates[Random.Range(0, candidates.Length)];
+    }
+
+    public T[] Pick(T[] candidates, int count, bool duplication)
+    {
+        if (count > candidates.Length) count = candidates.Length;
+
+        if (duplication)
+        {
+            List<T> pickedList = new List<T>();
+            for (int i = 0; i < count; i++)
+            {
+                T picked = candidates[Random.Range(0, candidates.Length)];
+                if (!pickedList.Contains(picked)) pickedList.Add(picked);
+            }
+
+            return pickedList.ToArray();
+        }
+        else
+        {
+            List<T> tmpCandidates = new List<T>(candidates);
+            List<T> pickedList = new List<T>();
+
+            for (int i = 0; i < count; i++)
+            {
+                int index = Random.Range(0, tmpCandidates.Count);
+                pickedList.Add(tmpCandidates[index]);
+                tmpCandidates.RemoveAt(index);
+            }
+
+            return pickedList.ToArray();
+        }
+    }
+}
